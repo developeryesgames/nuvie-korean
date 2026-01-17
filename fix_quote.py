5740daabdceda14cf1feb6f9f798086473b926b0
yesgames <developer@yesgamesonline.com>
2026-01-18 01:28:14 +0900

    Remove dev scripts and temp files from repo

--- fix_quote.py
+++
@@ -1,9 +0,0 @@
-with open('KoreanTranslation.cpp', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-# Fix the broken quotes - replace ''' with '\''
-content = content.replace("''' || trimmed.back()", "'\'' || trimmed.back()")
-
-with open('KoreanTranslation.cpp', 'w', encoding='utf-8') as f:
-    f.write(content)
-print("Fixed!")
